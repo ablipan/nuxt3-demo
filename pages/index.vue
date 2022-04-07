8578d178297ce44cda682f81277945ca7a35bd64
lipan <pan.li@zhaopin.com.cn>
2022-04-07 17:25:19 +0800

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>One</div>
+</template>
+<script lang="ts" setup>
+const token = useCookie('token', {
+  maxAge: 60 * 60 * 24 * 15, // 15天
+})
+token.value = 'tokenValue'
+
+await $fetch('/api/postApi', {
+  method: 'post',
+  headers: useRequestHeaders(['cookie']),
+});
+
+await $fetch('/api/postApi', {
+  method: 'post',
+  headers: useRequestHeaders(['cookie']),
+  body: {
+    foo: 'bar'
+  }
+});
+</script>
---
+++ server/api/postApi.ts
@@ -0,0 +1,12 @@
+import type { IncomingMessage } from 'http';
+import { useBody, useCookie } from 'h3';
+
+export default async (req: IncomingMessage) => {
+  await useBody(req)
+  
+  const token = useCookie(req, 'token');
+
+  console.log('token: ' + token)
+
+  return {};
+};
